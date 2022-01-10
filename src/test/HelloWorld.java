356f5ed3141ab3d16d4e00aac5005a1ef6d57454
wwk <1565308101@qq.com>
2022-01-10 15:00:24 +0800

    first commit

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * @author 王文凯
+ * @create 2022/1/10,14:53
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
